c886921515c8f29bf4067c4c1a02cef1f690bf6f
Nur Aleyna Pektaş <nuraleynaaaa@gmail.com>
2025-03-17 15:51:45 +0300

    update

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-/*import izitoast from "izitoast";
+window.global = window;
+
+import izitoast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
 
 const form = document.getElementById("search-form");
 const input = document.getElementById("search-input");
@@ -72,79 +72,9 @@ function renderGallery(images) {
 
   gallery.innerHTML = markup;
 
-  const lightbox = new SimpleLightbox(".gallery-item", {
+  const lightbox = new window.SimpleLightbox(".gallery-item", {
     captionsData: "alt",
     captionDelay: 250,
   });
   lightbox.refresh();
-}
-*/
-window.global = window;
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
-import izitoast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
-const form = document.getElementById("search-form");
-const input = document.getElementById("search-input");
-const gallery = document.getElementById("gallery");
-const loadingSpinner = document.getElementById("loading-spinner");
-const API_KEY = "49228326-f0295c59acbd8047419a0b87e";
-
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const query = input.value.trim();
-  if (query === "") return;
-  gallery.innerHTML = "";
-  loadingSpinner.classList.remove("hidden");
-  const url = `https://pixabay.com/api/?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`;
-  fetch(url)
-    .then((response) => {
-      return response.json();
-    })
-    .then((images) => {
-      if (images.hits.length === 0) {
-        izitoast.error({
-          message:
-            "Sorry, there are no images matching your search query. Please try again!",
-        });
-      } else {
-        renderGallery(images.hits);
-      }
-    })
-    .finally(() => {
-      loadingSpinner.classList.add("hidden");
-    });
-});
-function renderGallery(images) {
-  const markup = images
-    .map(
-      ({
-        webformatURL,
-        largeImageURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      }) => {
-        return `
-      <a href="${largeImageURL}" class="gallery-item">
-        <img src="${webformatURL}" alt="${tags}" />
-        <div class="info">
-          <p>Likes: ${likes}</p>
-          <p>Views: ${views}</p>
-          <p>Comments: ${comments}</p>
-          <p>Downloads: ${downloads}</p>
-        </div>
-      </a>
-    `;
-      }
-    )
-    .join("");
-  gallery.innerHTML = markup;
-  // const lightbox = new window.SimpleLightbox(".gallery-item", {
-  //   captionsData: "alt",
-  //   captionDelay: 250,
-  // });
-  // lightbox.refresh();
 }
